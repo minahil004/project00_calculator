4db85f3ddb9619e4bd1a05578359c08c6e73d313
KashfUlHudaa <48785350+KashfUlHudaa@users.noreply.github.com>
2024-04-12 22:19:43 +0500

    first commit. Code for simple calclator

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+//Program 1 Simple Command Line Operator
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter number 1", type: "number", name: "firstNumber" },
+    {message: "Enter number 2", type:"number", name: "secondNumber"},
+    {mesage: "select operator",
+     type:"list", 
+     name:"operator", 
+     choices:["Addition", "Subtration", "Multiplication", "Division"],
+    },
+]);
+
+if(answer.operator === "Addition"){
+    console.log( answer.firstNumber + answer.secondNumber);
+}
+else if(answer.operator === "Subtraction"){
+    console.log( answer.firstNumber - answer.secondNumber);
+}
+else if(answer.operator === "Multiplication"){
+    console.log( answer.firstNumber * answer.secondNumber);
+}
+else if(answer.operator==="Division") {
+    console.log( answer.firstNumber / answer.secondNumber);
+}else {
+    console.log("Please select a valid operator");
+}
